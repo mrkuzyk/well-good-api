586d5dd84f01f51013b533d6ca57b1468e7e2f7b
Marian Kuzyk <mariankuzyk@gmail.com>
2023-06-21 20:32:40 +0300

    start with env setings

---
+++ app.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(DB_HOST)
+  .then(() => console.log("database success 213243"))
+  .catch(error => console.log(error.message))
+
+console.log(DB_HOST);
+
+// const express = require('express');
+// const logger = require('morgan');
+// const cors = require('cors');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+// require('dotenv').config();
+
+// const authRouter = require('./src/routes/api/auth');
+// const userRouter = require('./src/routes/api/user');
+// const bookRouter = require('./src/routes/api/book');
+// const statisticsRouter = require('./src/routes/api/statistics');
+// const quoteRouter = require('./src/routes/api/quote');
+
+// const app = express();
+
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static('public'));
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api/auth', authRouter);
+// app.use('/api/users', userRouter);
+// app.use('/api/book', bookRouter);
+// app.use('/api/statistics', statisticsRouter);
+// app.use('/api/quote', quoteRouter);
+
+// app.use((_, res) => {
+//   res.status(404).json({ message: 'Not found' });
+// });
+
+// app.use((err, _, res, next) => {
+//   const { status = 500, message = 'Server error' } = err;
+//   res.status(status).json({ message });
+// });
+
+// module.exports = app;
---
+++ server.js
@@ -0,0 +1,25 @@
+// const app = require('./app')
+
+// app.listen(3001, () => {
+//     console.log("Server running. Use our API on port: 3000")
+// })
+
+// const mongoose = require('mongoose');
+
+// const app = require('./app');
+
+// require('dotenv').config();
+
+// const { DB_HOST, PORT = 3001 } = process.env;
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() =>
+//     app.listen(PORT, () => {
+//       console.log(`Database connection successful ${PORT}`);
+//     })
+//   )
+//   .catch(error => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
